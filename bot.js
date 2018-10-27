391ba12479dff325046ddbb23472e62c8c06566e
baadr11 <44349375+baadr11@users.noreply.github.com>
2018-10-27 18:15:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3083,8 +3083,7 @@ client.on('message', message => {
    if(!message.channel.guild) return;
 if(message.content.startsWith(prefix + 'bc')) {
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-if(!message.member.hasPermission('ADMINI
-STRATOR')) return
+if(!message.member.hasPermission('ADMINISTRATOR')) return
 message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
 const args = message.content.split(" ").slice(1).join(" ")
 const BcList = new Discord.RichEmbed()
