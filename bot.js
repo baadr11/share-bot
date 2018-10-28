407565df8bfcd4b5f81cae7e773e85a6673b8840
baadr11 <44349375+baadr11@users.noreply.github.com>
2018-10-28 04:44:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,17 +2,6 @@ const Discord = require('discord.js');
 const A7MD = new Discord.Client();
 
 console.log("BOT ONLINE");
- 
-
-A7MD.on("ready", async () => {
-    if(A7MD.guilds.size > 1) {
-        A7MD.user.setActivity(`Konex.`);
-        console.log(`${A7MD.user.username} is online on ${A7MD.guilds.size} servers!`)
-    } else {
-        A7MD.user.setActivity(`Konex.`);
-        console.log(`${A7MD.user.username} is online on ${A7MD.guilds.size} server!`)
-    }
-    A7MD.user.setStatus("Idle");
 
 A7MD.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
@@ -25,6 +14,7 @@ A7MD.on("guildMemberAdd", member => {
 تـنورنا :yellow_heart: ... [ ${member}  ]
 **`) 
 }).catch(console.error)
+})
 });
 
 
