22a125e7c7475998c6a065b47cb0271832ca106e
baadr11 <44349375+baadr11@users.noreply.github.com>
2018-11-22 01:45:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,8 +74,8 @@ client.on('message', message => {
             if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('$bcall')){
- if (message.author.id !== '342288364361482253') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
- if(!message.author.id === '342288364361482253') return;
+ if (message.author.id !== '496666381488619532') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if(!message.author.id === '496666381488619532') return;
 message.channel.sendMessage('جار ارسال الرسالة |✅')
 client.users.forEach(m =>{
 m.sendMessage(args)
